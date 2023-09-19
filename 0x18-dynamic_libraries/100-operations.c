61d3740032c8afda38c5a6d4fde0ba046555e493
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2023-09-19 08:23:27 +0300

    late 2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int add(int a, int b){
-	return a+b;
+        return a+b;
 }
 int sub(int a, int b){
-	return a-b;
+        return a-b;
 }
 int mul(int a,int b){
-	return a*b;
+        return a*b;
 }
 
 int div(int a, int b){
-	if(b ==0){
-		printf("Error: Division by zero\n");
-		return 0;
-	}
-	return a/b;
+        if(b ==0){
+                printf("Error: Division by zero\n");
+                return 0;
+        }
+        return a/b;
 }
 int mod (int a,int b){
         if(b ==0){
