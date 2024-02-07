f3440cbeed7a468f4feab0d738692b05ff0ebf3d
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-02-07 23:20:08 +0000

    Update time complexity in search algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,43 +1,41 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
+ * jump_search - searches for a value in an array of integers
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located or -1 on failure or not found
  */
+
 int jump_search(int *array, size_t size, int value)
 {
-	int index, m, k, prev;
+	size_t strt = 0, jmp, end = 0;
 
-	if (array == NULL || size == 0)
+	if (!array || size == 0)
 		return (-1);
 
-	m = (int)sqrt((double)size);
-	k = 0;
-	prev = index = 0;
+	jmp = sqrt(size);
 
-	do {
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
-
-		if (array[index] == value)
-			return (index);
-		k++;
-		prev = index;
-		index = k * m;
-	} while (index < (int)size && array[index] < value);
-
-	printf("Value found between indexes [%d] and [%d]\n", prev, index);
-
-	for (; prev <= index && prev < (int)size; prev++)
+	while (end < size && array[end] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
+		printf("Value checked array[%lu] = [%d]\n", end, array[end]);
+		end += jmp;
+	}
+
+	strt = end - jmp;
+	printf("Value found between indexes [%lu] and [%lu]\n", strt, end);
+
+	end = end >= size ? size - 1 : end;
+
+	while  (strt <= end)
+	{
+		printf("Value checked array[%lu] = [%d]\n", strt, array[strt]);
+		if (array[strt] == value)
+			return ((int)strt);
+		strt++;
 	}
 
 	return (-1);
--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,64 +1,78 @@
 #include "search_algos.h"
 
 /**
- * rec_search - searches for a value in an array of
- * integers using the Binary search algorithm
+ * print_array - prints the array of ints, separated with commas
  *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @array: a pointer to the array to print
+ * @first: the first index to print
+ * @last: the last index to print
  */
-int rec_search(int *array, size_t size, int value)
+
+void print_array(int *array, size_t first, size_t last)
 {
-	size_t half = size / 2;
 	size_t i;
 
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
+	printf("Searching in array: ");
+	for (i = first; i <= last; i++)
 	{
-		if (half > 0)
-			return (rec_search(array, half + 1, value));
-		return ((int)half);
+		if (i != first)
+			printf(", ");
+		printf("%d", array[i]);
 	}
-
-	if (value < array[half])
-		return (rec_search(array, half + 1, value));
-
-	half++;
-	return (rec_search(array + half, size - half, value) + half);
+	printf("\n");
 }
 
 /**
- * advanced_binary - calls to rec_search to return
- * the index of the number
+ * binary_rec - searches for a value in an array of integers recursively
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @array: a pointer to the first element of the array to search in
+ * @first: the minimum index of the array
+ * @last: the maximum index of the array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located or -1 on failure or not found
  */
+
+int binary_rec(int *array, size_t first, size_t last, int value)
+{
+	size_t i = 0;
+
+	if (array)
+	{
+		if (first <= last)
+		{
+			print_array(array, first, last);
+			i = (first + last) / 2;
+
+			if (array[first] == value)
+				return (first);
+
+			if (value == array[i] && array[i - 1] != value)
+				return (i);
+			if (value > array[i])
+				return (binary_rec(array, i + 1, last, value));
+			if (value <= array[i])
+				return (binary_rec(array, first, i, value));
+		}
+	}
+
+	return (-1);
+}
+
+/**
+ * advanced_binary - searches for a value in an array of integers
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located or -1 on failure or not found
+ */
+
 int advanced_binary(int *array, size_t size, int value)
 {
-	int index;
+	if (array)
+		return (binary_rec(array, 0, size - 1, value));
 
-	index = rec_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
+	return (-1);
 }
